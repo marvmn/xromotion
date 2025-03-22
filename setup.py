37218b0105b132fa1ee7764295adf2231b6c6bee
Marvin Wiebe <marvin.wiebe@uni-bielefeld.de>
2025-03-22 16:02:11 +0100

    added method to save keyframes

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
 packages=['expressive_motion_generation'],
+scripts=['src/execute_motion.py', 'src/save_keyframe.py'],
 package_dir={'': 'src'},
 )
 setup(**setup_args)
