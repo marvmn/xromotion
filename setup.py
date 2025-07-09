d685f5f036272bec9ce630b6054aee3f53ccc8a6
Marvin Wiebe <marvin.wiebe@uni-bielefeld.de>
2025-07-09 14:34:43 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
 packages=['expressive_motion_generation'],
-scripts=['src/execute_motion.py', 'src/save_keyframe.py'],
+scripts=[],
 package_dir={'': 'src'},
 )
 setup(**setup_args)
